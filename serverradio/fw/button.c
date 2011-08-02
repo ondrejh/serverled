979321c87caaf29d971e766838a29d3921416978
Ondrej Hejda <ondrejh.ck@gmail.com>
2011-08-02 14:58:21 +0200

    knihovna pro praci s tlacitky

---
+++ serverradio/fw/button.c
@@ -0,0 +1,110 @@
+/** button driver module */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <inttypes.h>
+#include <stdbool.h>
+
+#include "button.h"
+
+#define BTN_PRESS_TIME 20
+uint8_t btn_timer = 0;
+
+/// button down (if btn not exist return -1 else return 0)
+int8_t btn_down(uint8_t btn)
+{
+    switch (btn)
+    {
+        case 0:
+            DDRD |= 0x40;
+            return 0;
+            break;
+        case 1:
+            DDRD |= 0x80;
+            return 0;
+            break;
+        case 2:
+            DDRB |= 0x01;
+            return 0;
+            break;
+        default:
+            return -1;
+            break;
+    }
+}
+
+/// button up (if btn not exist return -1 else return 0)
+int8_t btn_up(uint8_t btn)
+{
+    switch (btn)
+    {
+        case 0:
+            DDRD &= ~0x40;
+            return 0;
+            break;
+        case 1:
+            DDRD &= ~0x80;
+            return 0;
+            break;
+        case 2:
+            DDRB &= ~0x01;
+            return 0;
+            break;
+        default:
+            return -1;
+            break;
+    }
+}
+
+/// press button loop function
+void press_button_wait(uint8_t btn)
+{
+    while (press_button(btn)!=0) {};
+}
+
+/** press button function
+    press and hold defined button for constant time */
+int8_t press_button(uint8_t btn)
+{
+    static uint8_t btn_pressed = 0;
+    static bool on_waiting = false;
+
+    if (on_waiting)
+    {
+        if (btn_timer==0) // wait timer
+        {
+            on_waiting = false;
+            btn_up(btn_pressed);
+            return 0; // indicate done
+        }
+    }
+    else
+    {
+        if (btn_down(btn)==0)
+        {
+            btn_pressed = btn;
+            btn_timer = BTN_PRESS_TIME;
+            on_waiting = true;
+        }
+        else return 0; // button couldnt be pressed
+    }
+
+    if (btn==btn_pressed) return 1; // indicate waiting
+    return -1; // error value
+}
+
+/// init button timer
+void init_timer(void)
+{
+    // init timer
+    TCCR0A = (2<<WGM00); // CTC
+    OCR0A = 71; // 7.3728MHz / 1024 / 72 = 100Hz
+    TIMSK0 = (1<<OCIE0A); // compare match interrupt
+    TCCR0B = (5<<CS00); // fosc/1024
+}
+
+/// timer interrupt
+SIGNAL (TIMER0_COMPA_vect)
+{
+    if (btn_timer) btn_timer--;
+}
---
+++ serverradio/fw/button.h
@@ -0,0 +1,15 @@
+/** button driver module header */
+
+#ifndef _BUTTON_H
+#define _BUTTON_H
+
+/// press button loop function
+void press_button_wait(uint8_t btn);
+
+/// press button function
+int8_t press_button(uint8_t btn);
+
+/// init button timer
+void init_timer(void);
+
+#endif
